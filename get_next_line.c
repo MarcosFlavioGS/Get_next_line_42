a1dd9ba5f4b0d0437ee122c9f6a04c0fb459601f
mflavio- <mfghost69@gmail.com>
2022-09-26 22:46:58 +0200

    Estruturando o raciocínio

--- get_next_line.c
+++ get_next_line.c
@@ -1,4 +1,39 @@
-char *get_next_line(int fd)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/26 20:11:48 by coder             #+#    #+#             */
+/*   Updated: 2022/09/26 21:46:42 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	read_line(int fd)
 {
-  char *line;
+	while (read(fd) != 0)
+	{
+		if (gnl_strchr('\n'))
+		{
+			return ;
+		}
+	}
+}
+
+char	*get_next_line(int fd)
+{
+  static char 	extra;
+  char			*line;
+  if (!fd || BUFFER_SIZE <= 0)
+	  return (NULL);
+  read_line();
+  if (extra)
+  {
+	  extra = get_extra();
+	  line = get_line;
+  }
+  if (!line)
+	  return (NULL);
+  return (line);
 }
