3857628cd595c0ffa56d4b3163d022c9bf02a53f
mflavio- <mfghost69@gmail.com>
2022-10-08 01:28:04 +0200

    Usando um poema do Blake pra me incentivar

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 00:47:27 by coder             #+#    #+#             */
-/*   Updated: 2022/10/08 00:54:27 by coder            ###   ########.fr       */
+/*   Updated: 2022/10/08 01:26:29 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,12 @@ int	main(void)
 	int 	fd;
 	char	*result = malloc(30);
 	//int   numbofchars;
-	fd = open("teste.txt", O_RDONLY);
+	fd = open("A_Poison_Tree_William_Blake.txt", O_RDONLY);
+	result = get_next_line(fd);
 	while (result != NULL)
 	{
-		result = get_next_line(fd);
 		printf("%s", result);
+		result = get_next_line(fd);
 	}
 	close(fd);
 	return (0);
